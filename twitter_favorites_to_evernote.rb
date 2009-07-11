8c84273b05f0b34c0153f36b3195873225d0d7fe
Albert Davidson Chou <HotFusionMan@Yahoo.com>
2009-07-11 09:39:46 -0700

    Wrapped all Twitter calls in rescue blocks, because Twitter::NotFound has been happening on occasion and was not being handled.

--- twitter_favorites_to_evernote.rb
+++ twitter_favorites_to_evernote.rb
@@ -39,19 +39,31 @@ base = Twitter::Base.new( httpauth )
 #page_number = 1
 loop do
   begin
-    page = base.favorites #( :page => page_number ) # Given that we're going to un-favorite tweets as we go, it only makes sense to always retrieve the newest page of favorites, which will be progressively older tweets as we go along.
+    begin
+      page = base.favorites #( :page => page_number ) # Given that we're going to un-favorite tweets as we go, it only makes sense to always retrieve the newest page of favorites, which will be progressively older tweets as we go along.
+    rescue Twitter::NotFound => e
+      puts "#{e.message} on tweet #{tweet.id}; sleeping for 5 seconds before retrying"
+      sleep 5
+      retry
+    end
 
     unless page.empty?
       page.each { |tweet|
         begin
           post_to_Evernote tweet.id.to_s, linkify( REXML::Text.new( tweet.text ).to_s )
-        rescue
-          puts "Had a problem posting tweet #{tweet.id} to Evernote."
+        rescue => e
+          puts "Had a problem posting tweet #{tweet.id} to Evernote: #{e.message}"
           $! = TRUE
         end
 
         if $!.nil? then
-          base.favorite_destroy tweet.id
+          begin
+            base.favorite_destroy tweet.id
+          rescue Twitter::NotFound => e
+            puts "#{e.message} on tweet #{tweet.id}; sleeping for 5 seconds before retrying"
+            sleep 5
+            retry
+          end
         else
           $! = nil
         end
@@ -62,8 +74,8 @@ loop do
       break
     end
   rescue Twitter::RateLimitExceeded => e
-    minutes_unitl_rate_limit_is_reset = ( base.rate_limit_status.reset_time_in_seconds - Time.now.to_i ) / 60.0
-    puts "Sorry, we've exceeded the Twitter-imposed rate limit for accessing their service.  We'll have to wait #{minutes_unitl_rate_limit_is_reset} minutes before this account can access Twitter again."
+    minutes_until_rate_limit_is_reset = ( base.rate_limit_status.reset_time_in_seconds - Time.now.to_i ) / 60.0
+    puts "Sorry, we've exceeded the Twitter-imposed rate limit for accessing their service.  We'll have to wait #{minutes_until_rate_limit_is_reset} minutes before this account can access Twitter again."
     exit
   end
 end
