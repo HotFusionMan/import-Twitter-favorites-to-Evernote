580611a8826b6f1e608d43a3e19fd97bf790185f
Albert Davidson Chou <HotFusionMan@Yahoo.com>
2009-07-12 06:22:10 -0700

    Factored out linkifying code for reuse in linkifying postprocessor.

--- twitter_favorites_to_evernote.rb
+++ twitter_favorites_to_evernote.rb
@@ -10,21 +10,7 @@ require 'config_store'
 require 'twitter'
 require 'post_Evernote_note'
 require 'rexml/document'
-
-
-# http://snippets.dzone.com/posts/show/7455, ported from http://snippets.dzone.com/posts/show/6156
-@generic_URL_regexp = Regexp.new( '(^|[\n (\[{])([\w]+?://[\w]+[^ \"\n\r\t<]*)', Regexp::MULTILINE | Regexp::IGNORECASE )
-@starts_with_www_regexp = Regexp.new( '(^|[\n (\[{])((www)\.[^ \"\t\n\r<]*)', Regexp::MULTILINE | Regexp::IGNORECASE )
-@starts_with_ftp_regexp = Regexp.new( '(^|[\n (\[{])((ftp)\.[^ \"\t\n\r<]*)', Regexp::MULTILINE | Regexp::IGNORECASE )
-@email_regexp = Regexp.new( '(^|[\n ])([a-z0-9&\-_\.]+?)@([\w\-]+\.([\w\-\.]+\.)*[\w]+)', Regexp::IGNORECASE )
-def linkify( text )
-  s = text.to_s
-  s.gsub!( @generic_URL_regexp, '\1<a href="\2">\2</a>' )
-  s.gsub!( @starts_with_www_regexp, '\1<a href="http://\2">\2</a>' )
-  s.gsub!( @starts_with_ftp_regexp, '\1<a href="ftp://\2">\2</a>' )
-  s.gsub!( @email_regexp, '\1<a href="mailto:\2@\3">\2@\3</a>' )
-  s
-end
+require 'linkify'
 
 
 twitter_config = ConfigStore.new( "#{ENV['HOME']}/.twitter" )
